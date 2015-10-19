dae8dd6216737f6d987489ae30201e886e04610c
KenMayorga <kmayorga02@gmail.com>
2015-10-19 16:00:34 -0700

    Update my_unix_commands.h

--- bbfs/fuse-tutorial-2014-06-12/src/my_unix_commands.h
+++ bbfs/fuse-tutorial-2014-06-12/src/my_unix_commands.h
@@ -2,11 +2,7 @@ int my_lstat(const char *path, struct stat *buf);
 
 int my_readlink(const char *path, char *buf, size_t bufsiz);
 
-int my_open(const char *pathname, int flags, mode_t mode);
-
-int my_close(int fd);
-
-int my_mkfifo(const char *pathname, mode_t mode);
+int my_close(ino_t fh);
 
 int my_mknode(const char *pathname, mode_t mode, dev_t dev);
 
@@ -32,19 +28,19 @@ int my_utime(const char *filename, const struct utimbuf *times);
 
 int my_open(const char *pathname, int flags);
 
-int my_pread(int fd, void *buf, size_t count, off_t offset);
+int my_pread(int fh, void *buf, size_t count, off_t offset);
 
-int my_pwrite(int fd, void *buf, size_t count, off_t offset);
+int my_pwrite(int fh, void *buf, size_t count, off_t offset);
 
 int my_statvfs(const char *path, struct statvfs *buf);
 
-int my_fdatasync(int fd);
+int my_fdatasync(ino_t fh);
 
-int my_fsync(int fd);
+int my_fsync(ino_t fh);
 
-int my_lsetxattr(const char *path, const char *name, const void *value, size_t size, int flags);
+int my_lsetxattr(const char *path, const char *name, const char *value, size_t size, int flags);
 
-int my_lgetxattr(const char *path, const char *name, const void *value, size_t size);
+int my_lgetxattr(const char *path, const char *name, const char *value, size_t size);
 
 int my_llistxattr(const char *path, char *list, size_t size);
 
