3bbd857f522bd49b6177a0b97f201ae80656d997
KenMayorga <kmayorga02@gmail.com>
2015-10-19 17:39:08 -0700

    Update my_structs.h

--- my_structs.h
+++ my_structs.h
@@ -1,4 +1,4 @@
-struct struct DIR {
+struct struct dirent {
     ino_t          d_ino
     off_t          d_off;       /* offset to the next dirent */
     unsigned short d_reclen;    /* length of this record */
