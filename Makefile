a6a5d1e81742f7e11754a6872bd65d1e078178c5
johndo007 <jdo007@ucr.edu>
2015-11-21 18:22:28 -0800

    Cleaning up unnecessary files

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ a.out: my_stubs.cc
 	g++ my_stubs.cc
 	
 clean:
-	rm -rf a.out
+	rm -rf a.out *.h.gch *.o
--- dirent.h
+++
@@ -1,8 +0,0 @@
-struct dirent {
-    ino_t          d_ino
-    off_t          d_off;       /* offset to the next dirent */
-    unsigned short d_reclen;    /* length of this record */
-    unsigned char  d_type;      /* type of file; not supported
-                                  by all file system types */
-    char           d_name[256]; /* filename */
-};
